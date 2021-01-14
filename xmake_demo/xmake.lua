4c59d9c98fe1dd5a2385e54669d6c50b2b3f08b8
huguanghui <522146829@qq.com>
2021-01-14 10:51:14 +0800

    feat(xmake): 优化 xmake 的结构体

--- xmake_demo/xmake.lua
+++ xmake_demo/xmake.lua
@@ -1,10 +1,11 @@
 add_rules("mode.debug", "mode.release")
 
 -- 引入自定义工具链
-includes("toolchains/arm-himix200-linux.lua")
+includes("toolchains/*.lua")
 
-target("cpu_test")
-    set_kind("binary")
-    add_files("src/*.c")
-    -- set_toolchains("arm-himix200-linux")
+option("nng_sdk")
+   set_default("$(projectdir)/third_part/nng")
+   set_showmenu(true)
+   set_description("NNG SDK")
 
+includes("demo")
