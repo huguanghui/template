b289a4b16bbe4a3c99e02637eeacd8a072c9ffde
huguanghui <522146820@qq.com>
2021-04-01 07:29:48 +0800

    fix(xmake): 去除冗余的xmake的demo目录

--- xmake_demo/gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xmake f -p linux -a arm --toolchain=arm-himix200-linux -m debug -c
-#xmake f -p linux -m debug
-
--- xmake_demo/toolchains/arm-himix200-linux.lua
+++
@@ -1,5 +0,0 @@
-toolchain("arm-himix200-linux")
-    set_kind("standalone")
-    set_sdkdir("/opt/hisi-linux/x86-arm/arm-himix200-linux")
-    set_bindir("/opt/hisi-linux/x86-arm/arm-himix200-linux/bin")
-toolchain_end()
