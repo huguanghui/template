594ade181a43d57aa36102ff2f365850da1ec828
huguanghui <522146829@qq.com>
2021-04-29 07:28:12 +0800

    feat(sh): refresh

--- shell/aml_strip.sh
+++ shell/aml_strip.sh
@@ -2,5 +2,11 @@
 
 TARGET_PATH=/root/amlogic/output/c2_af401_a6432_nand_release/target
 
+# 普通库和进程
+#   *-srip --remove-section=.comment --remove-section=.note
+# ld-*.so*
+#   *-srip --remove-section=.comment --remove-section=.note --strip-debug
+# 驱动
+#   *-srip --strip-debug
 find ${TARGET_PATH} -not \( -name 'iv009_isp' \) -type f \( -perm /111 -o -name '*.so.*' \) \
-    -not \( -name 'libpthread*.so*' -o -name 'ld-*.so*' -o -name '*.ko' \)
+    -not \( -name 'libpthread*.so*' -o -name 'ld-*.so*' -o -name '*.ko' \) -print0 | xargs -0 echo
