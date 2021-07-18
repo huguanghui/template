f84f591b970bd561d2461a5bb04c281333b19b3c
huguanghui <522146829@qq.com>
2021-07-18 20:52:28 +0800

    feat(xmake) 更新xmake的样例demo

--- xmake_cross/xmake.lua
+++ xmake_cross/xmake.lua
@@ -7,16 +7,11 @@ includes("toolchains/*.lua")
 option("build_type")
     -- set_default("3516DV300")
     -- set_values("3516DV300", "3516EV200")
-    set_default("AMLOGIC_C305X")
+    -- set_default("AMLOGIC_C305X")
+    set_default("LOCAL")
     set_showmenu(true)
     set_description("set build system type")
-    set_values("AMLOGIC_C305X")
-option_end()
-
-option("nng_sdk")
-   set_default("$(projectdir)/third_part/nng")
-   set_showmenu(true)
-   set_description("NNG SDK")
+    -- set_values("AMLOGIC_C305X")
 option_end()
 
 option("sdk_3516dv300")
@@ -40,15 +35,5 @@ if is_config("build_type", "AMLOGIC_C305X") then
     set_toolchains("aml-arm-linux")
     includes("demo")
 end
--- if is_config("build_type", "3516DV300") then
---    set_toolchains("arm-himix200-linux")
---    includes("mpp")
---    includes("src")
---end
 --
---if is_config("build_type", "3516EV200") then
---    set_toolchains("arm-himix100-linux")
---    set_targetdir("$(projectdir)/build/$(build_type)/$(mode)")
---   includes("codec")
--- end
 includes("src")
