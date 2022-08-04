f52a5daf0ea467ef51459f3eb4c8da4db1c67230
huguanghui <522146829@qq.com>
2022-08-04 08:50:57 +0800

    feat(cmake_c) 新增模块调用

--- cmake_c++/linux_c/src/main.c
+++ cmake_c++/linux_c/src/main.c
@@ -1,7 +1,67 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+
+static sem_t main_run_sem;
+int g_runiing = 0;
+
+static void signal_hander(int signal)
+{
+    if (signal == SIGINT) {
+        sem_post(&main_run_sem);
+    } else if (signal == SIGKILL) {
+        sem_post(&main_run_sem);
+    } else if (signal == SIGSEGV) {
+        SIG_DFL(signal);
+    }
+    return;
+}
+
+static void sig_chld(int signo)
+{
+    pid_t pid;
+    int stat;
+
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+        //   printf("child %d terminated\n", pid);
+    }
+    return;
+}
+
+static void capture_all_signal()
+{
+    int i = 0;
+    for (i = 0; i < 32; i++) {
+        if (i == SIGCHLD) {
+            // signal(i, SIG_DFL);
+            signal(i, sig_chld);
+        } else if ((i == SIGPIPE) || (i == SIGALRM)) {
+            signal(i, SIG_IGN);
+        } else {
+            signal(i, signal_hander);
+        }
+    }
+    return;
+}
 
 int main(int argc, char *argv[])
 {
+    capture_all_signal();
     printf("[HGH-TEST][%s %d] test\n", __FUNCTION__, __LINE__);
+    sem_init(&main_run_sem, 0, 0);
+
+    sem_wait(&main_run_sem);
+    int timer = 0;
+    while (1) {
+        timer++;
+        if (timer > 100) {
+            break;
+        }
+        usleep(10 * 1000);
+    }
+    sem_destroy(&main_run_sem);
+
     return 0;
 }
