fedba3f44ed5dd31fc6beb44177da4b367a69987
huguanghui <guanghui.hu@simshine.ai>
2022-09-13 17:05:22 +0800

    feat(shell) 支持JZ的openssl编译

---
+++ shell/openssl_crosscompile_jz.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env sh
+
+echo "cross compile ..."
+ROOT_PATH=$(pwd)
+CROSS_COMPILE=mips-linux-uclibc-gnu-gcc
+CROSS_HOST=mips-linux-gn
+CROSS_PREFIX=mips-linux-uclibc-gnu-
+
+OPENSSL_URL=https://www.openssl.org/source/old/1.1.1/openssl-1.1.1q.tar.gz
+OPENSSL_PACKAGE=openssl-1.1.1q.tar.gz
+OPENSSL_DIR=openssl-1.1.1q
+
+remote_get_content() {
+  wget "$1"
+  #    if curl --version >/dev/null 2>&1
+  #    then
+  #        curl -fSL "$1"
+  #    elif wget --version >/dev/null 2>&1
+  #    then
+  #        wget "$1" -0 -
+  #    fi
+}
+
+# "Prepare Package"
+
+if [ ! -e $OPENSSL_DIR ]; then
+  echo "$OPENSSL_DIR not found"
+  if [ ! -e $OPENSSL_PACKAGE ]; then
+    echo "$OPENSSL_PACKAGE not found"
+    remote_get_content "$OPENSSL_URL"
+  else
+    echo "$OPENSSL_PACKAGE found"
+  fi
+  tar -xvf $OPENSSL_PACKAGE
+else
+  echo "$OPENSSL_DIR found"
+fi
+
+# Config
+# 4.运行配置
+cd $ROOT_PATH/$OPENSSL_DIR
+if [ -d $ROOT_PATH/$OPENSSL_DIR/.tmp ]; then
+  rm -rf $ROOT_PATH/$OPENSSL_DIR/.tmp
+fi
+mkdir -p $ROOT_PATH/$OPENSSL_DIR/.tmp
+./Configure no-asm no-shared no-async no-threads linux-mips32 --cross-compile-prefix=mips-linux-uclibc-gnu- --prefix=$(pwd)/_install
+sed -i "s/ -m64//g" ./Makefile
+
+# Build
+make
+make install
