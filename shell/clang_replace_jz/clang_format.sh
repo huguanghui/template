bde608ca5a4a697b64c7a3634c6f6f491ff6bf88
huguanghui <522146829@qq.com>
2022-09-30 00:31:08 +0800

    feat(clangd) jz编译器转换

---
+++ shell/clang_replace_jz/clang_format.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -f ./compile_commands.json ];then
+    echo "compile_commands.json exited"
+    sed -i "s/\"cc\"/\"mips-linux-uclibc-gnu-gcc\"/g" ./compile_commands.json
+    sed -i "s/-fno-ipa-sra//g" ./compile_commands.json
+    sed -i "s/-fno-var-tracking-assignments//g" ./compile_commands.json
+    sed -i "s/-mno-thumb-interwork//g" ./compile_commands.json
+    sed -i "s/-fconserve-stack//g" ./compile_commands.json
+fi
+
