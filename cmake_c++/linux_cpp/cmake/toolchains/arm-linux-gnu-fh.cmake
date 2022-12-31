c611dcc234a4e2d499ecb41cb4379fefe687b519
huguanghui <522146829@qq.com>
2022-12-31 23:22:03 +0800

    feat(cmake) 优化编译链

--- cmake_c++/linux_cpp/cmake/toolchains/arm-linux-gnu-fh.cmake
+++ cmake_c++/linux_cpp/cmake/toolchains/arm-linux-gnu-fh.cmake
@@ -13,10 +13,6 @@ set(CMAKE_ASM_COMPILER
     /opt/fullhan/toolchain/arm-fullhanv3-linux-uclibcgnueabi-b6/bin/arm-fullhanv3-linux-uclibcgnueabi-gcc
     CACHE FILEPATH "ASM compiler")
 
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -muclibc -march=mips32r2")
-# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -muclibc -march=mips32r2")
-# set(CMAKE_LD_FLAGS "${CMAKE_CXX_FLAGS} -muclibc")
-
 # Search libraries only under *target* paths.
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
