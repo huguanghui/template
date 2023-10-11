008b4864f76c28c15dcc1de688da73310379c5f5
huguanghui <522146829@qq.com>
2023-10-11 23:13:18 +0800

    feat(cmake) 优化cmake模板

--- cmake_c++/linux_cpp/build.sh
+++ cmake_c++/linux_cpp/build.sh
@@ -2,21 +2,18 @@
 
 SOURCE_DIR=$(pwd)
 
-BUILD_TYPE_ARRAY="debug"
+BUILD_TYPE_ARRAY="debug release"
 for i in ${BUILD_TYPE_ARRAY}; do
   BUILD_TYPE=${i}
   INSTALL_DIR=output/${BUILD_TYPE}
-  # INSTALL_DIR=/home/hgh/workspace/nfs/t31x
+  #INSTALL_DIR=/home/hgh/workspace/nfs/jz_webrtc
   BUILD_DIR=build/${BUILD_TYPE}
 
   cmake -H. -B${BUILD_DIR} \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-    # -DCMAKE_TOOLCHAIN_FILE=tools/cmake/toolchains/mips-linux-gnu-t31_uclibc_gcc472.cmake
-  # -DCMAKE_TOOLCHAIN_FILE=tools/cmake/toolchains/mips-linux-gnu-t31_gcc540.cmake
-  # -DPLATFORM_SDK_DIR=<PATH_TO_SDK> \
+    # -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/arm-linux-gnu-sv823c_glibc_gcc750.cmake
   cmake --build ${BUILD_DIR} -j8
   cmake --install ${BUILD_DIR}
 done
-
